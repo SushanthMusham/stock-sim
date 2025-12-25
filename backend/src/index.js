1eccf1c7c3ce35b0d715deed435651e33cd73ac7
DORAEMONlovesMICHAN <mushamsushanth@gmail.com>
2025-12-25 11:24:27 +0530

    updated cors with *

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,13 +14,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true
 }));
 
